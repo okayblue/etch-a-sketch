440357d3d3e1f14d3624d46175cdc1ae26f9b5b8
okayblue <56899411+okayblue@users.noreply.github.com>
2022-05-17 18:25:31 -0400

    Add function to clear the grid

--- script.js
+++ script.js
@@ -3,39 +3,45 @@ const button = document.querySelector('#squaresBtn');
 let num;
 let squares = document.querySelectorAll('#square');
 
-// ask for a grid size
+// create initial grid
+createGrid(16);
+
+// ask for a grid size when button is clicked
 button.addEventListener('click', () => {
     num = prompt('Enter number:');
     createGrid(num);
 })
 
 function createGrid(number) {
+    // clear grid before creating new grid
+    clearGrid();
+
     for (let i = 0; i < number * number; i++) {
         let div = document.createElement('div');
         div.setAttribute('id', 'square');
         div.setAttribute('style', 'flex-basis: ' + (100 / number) + '%');
         gridContainer.appendChild(div);
     };
-    squares = document.querySelectorAll('#square');
+    
     coloring();
 }
 
-
-// for (let i = 0; i < 16 * 16; i++) {
-//     let div = document.createElement('div');
-//     div.setAttribute('id', 'square');
-//     gridContainer.appendChild(div);
-// };
-
-
 // // mouseenter event to change div color
 function coloring() {
+    squares = document.querySelectorAll('#square');
+
     squares.forEach((square) => {
         square.addEventListener('mouseenter', () => {
             square.style.backgroundColor = "black";
         });
     });
 
+}
+
+function clearGrid() {
+    squares.forEach((square) => {
+        gridContainer.removeChild(square);
+    });
 }
 
 
